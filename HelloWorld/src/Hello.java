e3864d66ff8ca15f243fe1677b3739efda8f5237
Emiliano Hotes <emiliano.hotes@despegar.com>
2023-05-16 09:46:08 -0300

    feat: conditionals - 1st exercise

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -21,5 +21,15 @@ public class Hello {
         if (topScore > 90 || secondTopScore <= 90) {
             System.out.println("Either or both of the conditions are true");
         }
+
+        int newValue = 50;
+        if (newValue == 50) {
+            System.out.println("Either or both of the conditions are true");
+        }
+
+        boolean isCar = false;
+        if(!isCar) {
+            System.out.println("This is not supposed to happen");
+        }
     }
 }
