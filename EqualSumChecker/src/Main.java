0f22f209b43db346a3932f7429291a821b166e26
Emiliano Hotes <emiliano.hotes@despegar.com>
2023-05-17 17:18:24 -0300

    feat: completed Coding Exercise 7: Equal Sum Checker

---
+++ EqualSumChecker/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(hasEqualSum(1, 1, 1));
+        System.out.println(hasEqualSum(1, 1, 2));
+        System.out.println(hasEqualSum(1, -1, 0));
+    }
+
+    public static boolean hasEqualSum(int x, int y, int z) {
+        return x + y == z;
+    }
+
+}
