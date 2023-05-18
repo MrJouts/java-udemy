cf48f852551dbdaa65e7d0d2805d210d0a451223
Emiliano Hotes <emiliano.hotes@despegar.com>
2023-05-18 14:16:50 -0300

    feat: changed to switch enhaced version

--- section6/Switch/src/Main.java
+++ section6/Switch/src/Main.java
@@ -3,17 +3,13 @@ public class Main {
         int value = 4;
 
         switch (value) {
-            case 1:
-                System.out.println("Value was 1");
-                break;
-            case 2:
-                System.out.println("Value was 2");
-            case 3: case 4: case 5:
+            case 1 -> System.out.println("Value was 1");
+            case 2 -> System.out.println("Value was 2");
+            case 3, 4, 5 -> {
                 System.out.println("Was a 3, a 4 or a 5");
                 System.out.println("Actually it was a " + value);
-                break;
-            default:
-                System.out.println("Was not 1, 2, 3, 4, or 5");
+            }
+            default -> System.out.println("Was not 1, 2, 3, 4, or 5");
         }
     }
 }
