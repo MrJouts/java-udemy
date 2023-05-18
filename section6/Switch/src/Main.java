90cba399755e34593e76e3d286beb31ff0f5ad2d
Emiliano Hotes <emiliano.hotes@despegar.com>
2023-05-18 16:39:11 -0300

    feat: changed getQuarter method to switch enhaced version

--- section6/Switch/src/Main.java
+++ section6/Switch/src/Main.java
@@ -12,29 +12,20 @@ public class Main {
             default -> System.out.println("Was not 1, 2, 3, 4, or 5");
         }
 
-        String month = "OCTOBER";
+        String month = "XYZ";
         System.out.println(month + " is in the " + getQuarter(month) + " quarter");
     }
 
     public static String getQuarter(String month) {
-        switch (month) {
-            case "JANUARY":
-            case "FEBRUARY":
-            case "MARCH":
-                return "1st";
-            case "APRIL":
-            case "MAY":
-            case "JUNE":
-                return "2nd";
-            case "JULY":
-            case "AUGUST":
-            case "SEPTEMBER":
-                return "3rd";
-            case "OCTOBER":
-            case "NOVEMBER":
-            case "DECEMBER":
-                return "4th";
-        }
-        return "bad";
+        return switch (month) {
+            case "JANUARY", "FEBRUARY", "MARCH" -> "1st";
+            case "APRIL", "MAY", "JUNE" -> "2nd";
+            case "JULY", "AUGUST", "SEPTEMBER" -> "3rd";
+            case "OCTOBER", "NOVEMBER", "DECEMBER" -> "4th";
+            default -> {
+                String badResponse = month + " is bad";
+                yield badResponse;
+            }
+        };
     }
 }
