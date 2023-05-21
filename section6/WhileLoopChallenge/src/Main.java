987ebe46ec933fa0f0830a3ca390d80ad4b84967
Emiliano Hotes <emiliano.hotes@despegar.com>
2023-05-21 19:46:40 -0300

    feat: While Loop Challenge - step 2

--- section6/WhileLoopChallenge/src/Main.java
+++ section6/WhileLoopChallenge/src/Main.java
@@ -1,11 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         int number = 4;
+        int eventCount = 0;
+        int oddCount = 0;
+
         while(number <= 20) {
             number++;
-            if(!isEvenNumber(number)) continue;
+            if(!isEvenNumber(number)) {
+                oddCount++;
+                continue;
+            }
             System.out.println(number);
+            eventCount++;
+            if(eventCount >= 5) break;
         }
+
+        System.out.println("Total even numbers found: " + eventCount);
+        System.out.println("Total odd numbers found: " + oddCount);
     }
 
     public static boolean isEvenNumber(int number) {
