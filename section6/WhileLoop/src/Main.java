b381d5d8b3c318048aad20a5c83eaea4b330e029
Emiliano Hotes <emiliano.hotes@despegar.com>
2023-05-21 10:25:48 -0300

    feat: The Do while Statetement

--- section6/WhileLoop/src/Main.java
+++ section6/WhileLoop/src/Main.java
@@ -6,9 +6,9 @@ public class Main {
         }
 
         int j = 1;
-        while (j <= 5) {
+        do {
             System.out.println(j);
             j++;
-        }
+        } while (j <= 5);
     }
 }
