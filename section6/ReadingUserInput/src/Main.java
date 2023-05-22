cdbad56413b95e802f04060eae01a7daeac3aef4
Emiliano Hotes <emiliano.hotes@despegar.com>
2023-05-22 18:31:00 -0300

    feat: Exception Handling and Introduction to Scanner

--- section6/ReadingUserInput/src/Main.java
+++ section6/ReadingUserInput/src/Main.java
@@ -3,8 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         int currentYear = 2022;
-        System.out.println(getInputFromConsole(currentYear));
-        System.out.println(getInputFromScanner(currentYear));
+
+        try {
+            System.out.println(getInputFromConsole(currentYear));
+        } catch (NullPointerException e) {
+            //System.out.println(e.getMessage());
+            System.out.println(getInputFromScanner(currentYear));
+        }
     }
 
     public static String getInputFromConsole(int currentYear) {
@@ -18,8 +23,15 @@ public class Main {
     }
 
     public static String getInputFromScanner(int currentYear) {
-        return "";
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hi, What's your Name? ");
+        String name = scanner.nextLine();
+        System.out.println("Hi " + name + ", Thanks for taking the course");
+
+        System.out.println("What year were you born? ");
+        String dateOfBirth = scanner.nextLine();
+        int age = currentYear - Integer.parseInt(dateOfBirth);
+        return "So you are " + age + " years old";
     }
-
-
 }
