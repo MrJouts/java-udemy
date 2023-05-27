0d1ab223744f1380757f364c7fc6bb5f5aa0670e
Emiliano Hotes <emiliano.hotes@despegar.com>
2023-05-27 20:31:33 -0300

    feat: Constructors Pat 1 - Creation of a constructor

--- section7/ClassesChallenge/src/Main.java
+++ section7/ClassesChallenge/src/Main.java
@@ -1,13 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        Account bobsAccount = new Account();
-
-        bobsAccount.setNumber("1239081293");
-        bobsAccount.setBalance(1000);
-        bobsAccount.setCustomerName("Bob Brown");
-        bobsAccount.setCustomerEmail("bob@hotmail.com");
-        bobsAccount.setCustomerPhone("7364-0833");
-
+        Account bobsAccount = new Account("123123123", 5000.00, "Bob Brown", "bob@hotmail.com", "9739-872");
+        System.out.println(bobsAccount.getNumber());
+        System.out.println(bobsAccount.getBalance());
         bobsAccount.depositFunds(500);
         bobsAccount.withdrawFunds(800);
         bobsAccount.withdrawFunds(2200);
