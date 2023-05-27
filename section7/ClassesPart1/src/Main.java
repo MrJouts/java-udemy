4100b998dd1081eb41e95e01392001aeb8f243ad
Emiliano Hotes <emiliano.hotes@despegar.com>
2023-05-27 10:31:44 -0300

    feat: Creating objets

--- section7/ClassesPart1/src/Main.java
+++ section7/ClassesPart1/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        Car car = new Car();
 
+        Car car = new Car();
         car.setMake("Porsche");
         car.setModel("Carrera");
         car.setDoors(2);
@@ -11,5 +11,16 @@ public class Main {
         System.out.println("make = " + car.getMake());
         System.out.println("model = " + car.getModel());
         car.describeCar();
+
+        Car targa = new Car();
+        targa.setMake("Porsche");
+        targa.setModel("Targa");
+        targa.setDoors(2);
+        targa.setConvertible(false);
+        targa.setColor("red");
+
+        targa.describeCar();
+
+
     }
 }
