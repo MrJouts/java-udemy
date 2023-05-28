344eb0c81ee6fe5d4631ffe6f3b98166b63f8b0f
Emiliano Hotes <emiliano.hotes@despegar.com>
2023-05-28 20:17:26 -0300

    feat: Java's Implicit POJO Type, The Record

--- section7/RecordProject/src/Main.java
+++ section7/RecordProject/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         for (int i = 1; i <= 5; i++) {
-            Student s = new Student("S92300" + i,
+            LPAStudent s = new LPAStudent("S92300" + i,
                     switch (i) {
                         case 1 -> "Mary";
                         case 2 -> "Carol";
@@ -13,8 +13,16 @@ public class Main {
                     },
                     "05/11/1985",
                     "Java Masterclass");
-
             System.out.println(s);
         }
+
+        Student pojoStudent = new Student("S923006", "Ann", "05/11/1985", "Java Masterclass");
+        LPAStudent recordStudent = new LPAStudent("S923006", "Bill", "05/11/1985", "Java Masterclass");
+
+        System.out.println(pojoStudent);
+        System.out.println(recordStudent);
+
+        System.out.println(pojoStudent.getName() + " is taking " + pojoStudent.getClassList());
+        System.out.println(recordStudent.name() + " is taking " + recordStudent.classList());
     }
 }
