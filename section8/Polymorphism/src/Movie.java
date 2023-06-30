5d099a369dc5faf0759b2f2c49187cd9bf73f69c
Emiliano Hotes <emiliano.hotes@despegar.com>
2023-06-30 17:18:30 -0300

    Casting with classes, and using Object and var references

--- section8/Polymorphism/src/Movie.java
+++ section8/Polymorphism/src/Movie.java
@@ -34,6 +34,10 @@ class Adventure extends Movie {
                 "Scary Music",
                 "Something Bad Happens");
     }
+
+    public void watchAdventure() {
+        System.out.println("Watching an Adventure!");
+    }
 }
 
 class Comedy extends Movie {
@@ -50,6 +54,10 @@ class Comedy extends Movie {
                 "Something even funnier happends",
                 "Happy Ending");
     }
+
+    public void watchComedy() {
+        System.out.println("Watching a Comedy!");
+    }
 }
 
 class ScienceFiction extends Movie {
@@ -65,5 +73,9 @@ class ScienceFiction extends Movie {
                 "Bad Aliens do Bad Stuff",
                 "Space Guys Chase Aliens",
                 "Planet Blows Up");
+    }
+
+    public void watchScienceFiction() {
+        System.out.println("Watching a Science Fiction Thriller!");
     }
 }
